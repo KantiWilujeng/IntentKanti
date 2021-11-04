a0e41dccff8eb1c455abec08f92d7de4c5ad32af
KantiWilujeng <kanti7wilujeng@gmail.com>
2021-11-04 23:55:08 +0700

    Intent Kanti

--- app/src/main/java/com/example/intentkanti/MainActivity.kt
+++ app/src/main/java/com/example/intentkanti/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnMoveActivity: Button
+    private lateinit var btnMoveWithData: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnMoveActivity = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        btnMoveWithData = findViewById(R.id.btn_move_with_data)
+        btnMoveWithData.setOnClickListener(this)
     }
 
     override fun onClick(v:View?) {
@@ -22,6 +26,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             when(v.id) {
                 R.id.btn_move_activity -> run {
                     val intent = Intent(this, MoveActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn_move_with_data -> run {
+                    val intent = Intent(this, MoveWithDataActivity::class.java)
+                    val bundle = Bundle()
+                    bundle.putString("Nama", "Kanti")
+                    bundle.putString("Alamat", "Jepang")
+                    intent.putExtras(bundle)
                     startActivity(intent)
                 }
             }
